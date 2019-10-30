2a85d2f63334a543b6674ad80a245d1f3e3058e6
Patrick <pc8229@exeter.ac.uk>
2019-10-30 09:56:26 +0000

    Moved setup to account profile

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '@app/_models';
+import { AuthenticationService } from '@app/_services/authentication.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+
+  constructor(
+    private authService: AuthenticationService
+  ) { }
 
   ngOnInit() {
+    this.user = this.authService.currentUserValue;
+    // Get latest user value
+    this.authService.getCurrent().pipe(take(1)).subscribe(user => {
+      this.user = user;
+    })
   }
 
 }
