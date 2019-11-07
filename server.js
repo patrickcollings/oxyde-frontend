c816d1a751d3aa4e1bebe825cf95f7b0b04b1b0c
Patrick <pc8229@exeter.ac.uk>
2019-11-07 09:36:37 +0000

    Removed additional force ssl url

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 app.use(forceSecure([
-    "oxydetechnologies.com",
+    // "oxydetechnologies.com",
     "dashboard.oxydetechnologies.com"
 ]));
 
